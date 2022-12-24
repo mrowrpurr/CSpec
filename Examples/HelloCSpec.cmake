e6264f6bca39f2d2683c19e0fe1db6f0d6f5339a
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-24 02:39:45 -0800

    Prototype complete. Under 80 LOC

--- Examples/HelloCSpec.cmake
+++ Examples/HelloCSpec.cmake
@@ -1,3 +1,11 @@
 function(test_something)
-    message("YOU CALLED THE test_something TEST FUNCTION!")
+    message("!!!!!!!!!! YOU CALLED THE test_something TEST FUNCTION!")
+endfunction()
+
+function(test_something_fail)
+    message(SEND_ERROR "!!!!!!!!!!!! This failed!")
+endfunction()
+
+function(test_something_xxx)
+    message("!!!!!!!!!!! YOU CALLED THE test_something_xxx TEST FUNCTION!")
 endfunction()
