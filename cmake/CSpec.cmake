47e2961d97e7b7b78bf47d4a2f8125b9bc478fd2
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-27 18:58:17 -0800

    Format file

--- cmake/CSpec.cmake
+++ cmake/CSpec.cmake
@@ -3,6 +3,5 @@ get_filename_component(CSpecModuleIncludes "${CSpecModuleIncludes}" DIRECTORY)
 string(APPEND CSpecModuleIncludes /CSpec)
 
 # TODO: cspec_configure(X y Y z)
-
 include("${CSpecModuleIncludes}/Common.cmake")
 include("${CSpecModuleIncludes}/AddCSpecSuite.cmake")
